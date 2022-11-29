2e2b2ae980778d7b1d10667dea08f4cc27f05c8a
pommevilla <pvillanueva13@gmail.com>
2022-11-28 19:09:35 -0600

    Add documentation

--- src/ispcr/utils.py
+++ src/ispcr/utils.py
@@ -2,6 +2,8 @@
 This module contains various utilities used during in silico PCR.
 """
 
+from typing import Iterator, List, TextIO, Tuple
+
 
 def reverse_complement(dna_string: str) -> str:
     """Returns the reverse complement of a DNA string.
@@ -19,6 +21,11 @@ def reverse_complement(dna_string: str) -> str:
     ------
     KeyError
         Raised if there is a base in dna_string that is not one of ACGT.
+
+    Example
+    -------
+    reverse_complement('GCTGA')
+    'TCAGC'
     """
 
     complements = {"A": "T", "C": "G", "G": "C", "T": "A"}
@@ -28,3 +35,37 @@ def reverse_complement(dna_string: str) -> str:
         print(f"Base {e.args} not supported.")
         raise
     return rev_seq
+
+
+def read_fasta(fasta_file: TextIO) -> Iterator[Tuple[str, str]]:
+    """An iterator for fasta files.
+
+    Inputs
+    ------
+    fasta_file: TextIO
+        An open file for reading
+
+    Outputs
+    -------
+    An iterator yielding the the sequence names and sequences from a fasta file
+
+    Example
+    -------
+    input_file = 'tests/test_data/sequences/met_r.fa.fasta'
+    with open(input_file) as fin:
+    for name, seq in read_fasta(fin):
+        print(f'{name}\n{seq}')
+
+    """
+    name = None
+    seq: List[str] = []
+    for line in fasta_file:
+        line = line.rstrip()
+        if line.startswith(">"):
+            if name:
+                yield (name, "".join(seq))
+            name, seq = line, []
+        else:
+            seq.append(line)
+    if name:
+        yield (name, "".join(seq))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,18 +1,34 @@
 from pytest import raises
 
-from ispcr.utils import reverse_complement
+from ispcr.utils import read_fasta, reverse_complement
 
 
-def test_reverse_complement() -> None:
-    test_string = "ACGT"
-    expect_reverse_complement = "ACGT"
-    actual_reverse_complement = reverse_complement(test_string)
+class TestReverseComplement:
+    def test_reverse_complement(self) -> None:
+        test_string = "ACGT"
+        expect_reverse_complement = "ACGT"
+        actual_reverse_complement = reverse_complement(test_string)
 
-    assert actual_reverse_complement == expect_reverse_complement
+        assert actual_reverse_complement == expect_reverse_complement
+
+    def test_incorrect_bases(self) -> None:
+        test_string = "AVGT"
+
+        with raises(KeyError):
+            reverse_complement(test_string)
 
 
-def test_incorrect_bases() -> None:
-    test_string = "AVGT"
+class TestReadFasta:
+    def test_correct_number_of_entries(self) -> None:
+        expected_num_lines = 20
+        input_file = "tests/test_data/sequences/met_r.fa.fasta"
+        import os
 
-    with raises(KeyError):
-        reverse_complement(test_string)
+        print(f"currently in {os.getcwd()}")
+
+        actual_num_lines = 0
+        with open(input_file) as fin:
+            for _name, _seq in read_fasta(fin):
+                actual_num_lines = actual_num_lines + 1
+
+        assert expected_num_lines == actual_num_lines
